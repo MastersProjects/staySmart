be20c043bf023c8fc449531317831fc7df24a3b8
zperee <elia.perenzin@bluewin.ch>
2019-07-16 14:59:06 +0200

    Fix broken e2e test

--- web/e2e/src/app.e2e-spec.ts
+++ web/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to staySmart!');
+    expect(page.getTitleText()).toEqual('Willkommen auf stay smart!');
   });
 
   afterEach(async () => {
