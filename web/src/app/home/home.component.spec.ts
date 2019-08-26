f6d9474da34fb8d95617d4986568227a73886b01
zperee <elia.perenzin@bluewin.ch>
2019-08-26 15:18:19 +0200

    F*** lint fixing

--- web/src/app/home/home.component.spec.ts
+++ web/src/app/home/home.component.spec.ts
@@ -1,14 +1,14 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {HomeComponent} from './home.component';
-import {AppRoutingModule} from '../app-routing.module';
-import {TutorSearchRequestComponent} from '../tutor-search-request/tutor-search-request.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
-import {OrganisationComponent} from '../other/organisation/organisation.component';
-import {PresseComponent} from '../other/presse/presse.component';
-import {TeamComponent} from '../other/team/team.component';
-import {TutorRegistrationComponent} from '../tutor-registration/tutor-registration.component'
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { TutorSearchRequestComponent } from '../tutor-search-request/tutor-search-request.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { OrganisationComponent } from '../other/organisation/organisation.component';
+import { PresseComponent } from '../other/presse/presse.component';
+import { TeamComponent } from '../other/team/team.component';
+import { TutorRegistrationComponent } from '../tutor-registration/tutor-registration.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -16,10 +16,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ AppRoutingModule, NgbModule, FormsModule ],
-      declarations: [ HomeComponent, TutorSearchRequestComponent, OrganisationComponent, PresseComponent, TeamComponent, TutorRegistrationComponent ]
+      imports: [AppRoutingModule, NgbModule, FormsModule],
+      declarations: [HomeComponent, TutorSearchRequestComponent, OrganisationComponent, PresseComponent,
+                TeamComponent, TutorRegistrationComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
