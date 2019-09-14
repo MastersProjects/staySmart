4b0bf704d1d84ecc892871940458288c2d9c967a
Chiramet (Phong) Penglerd <Phong@hotmail.ch>
2019-09-14 14:11:54 +0200

    linkRef for URL

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as nodemailer from 'nodemailer';
 import * as Mail from 'nodemailer/lib/mailer';
+import * as uuidv4 from 'uuid/v4';
 
 admin.initializeApp();
 
@@ -27,16 +28,20 @@ export const helloWorld = functions.region('europe-west1').https.onRequest((requ
 });
 
 export const emailOnSubmit = functions.region('europe-west1')
-    .firestore.document('TutorSearchRequests/{tutorSearchRequestID}').onCreate((snap) => {
+    .firestore.document('TutorSearchRequests/{tutorSearchRequestID}').onCreate(async (snap) => {
         console.log('emailOnSubmit');
         const createdTutorSearchRequest: any = snap.data();
         console.log(createdTutorSearchRequest);
+
+        const linkRef = uuidv4();
+
+        await snap.ref.update({linkRef: linkRef});
 
         const mailOptions: Mail.Options = {
             from: `StaySmart ${functions.config().env.code} <noreply-dev@staysmart.com>`,
             to: createdTutorSearchRequest.mail,
             subject: createdTutorSearchRequest.subject,
-            html: `<p style="font-size: 16px;">TEST ${snap.id}</p>
+            html: `<p style="font-size: 16px;">TEST https://staysmart-dev.web.app/search-request/${linkRef}</p>
                 <br />
                 ${createdTutorSearchRequest.problem}`
         };
