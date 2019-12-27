9e9acea68617ee6f2772fc17745362af227296f8
Chiramet (Phong) Penglerd <Phong@hotmail.ch>
2019-12-27 01:55:39 +0100

    removed unused FormsModule

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {LOCALE_ID, NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -47,7 +47,6 @@ registerLocaleData(localeDECH);
     AngularFireStorageModule,
     AngularFireAuthModule,
     NgbModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     FontAwesomeModule,
