207b26fcf167b5083ea54e7c46f56b66aa142189
Chiramet Phong Penglerd <Phong@hotmail.ch>
2020-02-11 18:05:43 +0100

    collapse animation ease-in-out

--- web/src/app/shared/collapse.animation.ts
+++ web/src/app/shared/collapse.animation.ts
@@ -3,6 +3,6 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 export const collapse = trigger('collapse', [
   state('closed', style({height: '0', overflow: 'hidden', opacity: '0'})),
   state('opened', style({overflow: 'hidden', opacity: '1'})),
-  transition('closed=>opened', animate('500ms')),
-  transition('opened=>closed', animate('500ms'))
+  transition('closed => opened', animate('500ms ease-in-out')),
+  transition('opened => closed', animate('500ms ease-in-out'))
 ]);
