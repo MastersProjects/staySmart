5c40ab9a71594264ced51a1b3eef71abdee3ed66
zperee <elia.perenzin@bluewin.ch>
2020-02-11 18:47:58 +0100

    fix test import errors

--- web/src/app/page/home/home.component.spec.ts
+++ web/src/app/page/home/home.component.spec.ts
@@ -5,9 +5,6 @@ import {AppRoutingModule} from '../../app-routing.module';
 import {TutorSearchRequestComponent} from '../tutor-search-request/tutor-search-request.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
-import {OrganisationComponent} from '../organisation/organisation.component';
-import {PresseComponent} from '../presse/presse.component';
-import {TeamComponent} from '../team/team.component';
 import {TutorRegistrationComponent} from '../tutor-registration/tutor-registration.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {StepperComponent} from '../../shared/stepper/stepper.component';
@@ -29,9 +26,6 @@ describe('HomeComponent', () => {
       declarations: [
         HomeComponent,
         TutorSearchRequestComponent,
-        OrganisationComponent,
-        PresseComponent,
-        TeamComponent,
         TutorRegistrationComponent,
         StepperComponent
       ]
