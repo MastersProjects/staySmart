8c8ed6a01583411fe62e1fda4e21fffd6b5e9a0a
Chiramet Phong Penglerd <Phong@hotmail.ch>
2020-06-03 13:38:10 +0200

    Fix Test: Admin-Portal Navigation & Refactoring [todo #38]

--- web/src/app/admin-portal/ap-tutor-list/ap-tutor-list.component.spec.ts
+++ web/src/app/admin-portal/ap-tutor-list/ap-tutor-list.component.spec.ts
@@ -1,6 +1,12 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ApTutorListComponent} from './ap-tutor-list.component';
+import {AdminPortalService} from '../shared/admin-portal.service';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFirePerformanceModule} from '@angular/fire/performance';
 
 describe('ApTutorListComponent', () => {
   let component: ApTutorListComponent;
@@ -8,9 +14,16 @@ describe('ApTutorListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ApTutorListComponent ]
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFirePerformanceModule,
+      ],
+      declarations: [ApTutorListComponent],
+      providers: [AdminPortalService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- web/src/app/admin-portal/shared/admin-portal.service.spec.ts
+++ web/src/app/admin-portal/shared/admin-portal.service.spec.ts
@@ -1,12 +1,25 @@
 import {TestBed} from '@angular/core/testing';
 
 import {AdminPortalService} from './admin-portal.service';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFirePerformanceModule} from '@angular/fire/performance';
 
 describe('AdminPortalService', () => {
   let service: AdminPortalService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFirePerformanceModule,
+      ],
+      providers: [AdminPortalService]
+    });
     service = TestBed.inject(AdminPortalService);
   });
 
