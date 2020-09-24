35ba1e18678838c9b257e8ace3ea6875792549e2
Chiramet Phong Penglerd <phong@hotmail.ch>
2020-09-24 17:51:26 +0200

    tests: tp-request-offer-form.component [todo #58]

--- web/src/app/tutor-portal/tp-request-offer-form/tp-request-offer-form.component.spec.ts
+++ web/src/app/tutor-portal/tp-request-offer-form/tp-request-offer-form.component.spec.ts
@@ -12,9 +12,9 @@ describe('TpRequestOfferFormComponent', () => {
       imports: [
         TestingModule,
       ],
-      declarations: [ TpRequestOfferFormComponent ]
+      declarations: [TpRequestOfferFormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,5 +25,46 @@ describe('TpRequestOfferFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('cancel()', () => {
+    it('should emit canceled event', () => {
+      spyOn(component.canceled, 'emit');
+
+      component.cancel();
+
+      expect(component.canceled.emit).toHaveBeenCalled();
+    });
+  });
+
+  describe('submitOffer()', () => {
+    it('should emit submitted requestOfferForm event on valid', () => {
+      spyOn(component.submitted, 'emit');
+      component.minPrice = 5;
+      component.ngOnInit();
+      component.requestOfferForm.setValue({
+        message: 'some message',
+        price: '8'
+      });
+      component.submitOffer();
+
+      expect(component.submitted.emit).toHaveBeenCalledWith({
+        message: 'some message',
+        price: '8'
+      });
+    });
+
+    it('should no emit submitted requestOfferForm event on invalid', () => {
+      spyOn(component.submitted, 'emit');
+      component.minPrice = 5;
+      component.ngOnInit();
+      component.requestOfferForm.setValue({
+        message: '',
+        price: '4'
+      });
+      component.submitOffer();
+
+      expect(component.submitted.emit).not.toHaveBeenCalled();
+    });
   });
 });
