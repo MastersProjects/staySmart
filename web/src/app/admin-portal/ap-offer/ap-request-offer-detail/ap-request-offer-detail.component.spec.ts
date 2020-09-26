b654c19a18fb398320a571deac801313457e0d77
Chiramet Phong Penglerd <phong@hotmail.ch>
2020-09-26 16:17:47 +0200

    tests: ap-request-offer-detail.component [todo #58]

--- web/src/app/admin-portal/ap-offer/ap-request-offer-detail/ap-request-offer-detail.component.spec.ts
+++ web/src/app/admin-portal/ap-offer/ap-request-offer-detail/ap-request-offer-detail.component.spec.ts
@@ -25,4 +25,66 @@ describe('ApRequestOfferDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('collapseToggle', () => {
+    it('should set true if false', () => {
+      component.isCollapsed = false;
+      component.collapseToggle();
+      expect(component.isCollapsed).toBeTruthy();
+    });
+
+    it('should set false if true', () => {
+      component.isCollapsed = true;
+      component.collapseToggle();
+      expect(component.isCollapsed).toBeFalsy();
+    });
+  });
+
+  describe('collapseState', () => {
+    it('should return `closed` if isCollapsed true', () => {
+      component.isCollapsed = true;
+      expect(component.collapseState).toBe('closed');
+    });
+
+    it('should return `opened` if isCollapsed false', () => {
+      component.isCollapsed = false;
+      expect(component.collapseState).toBe('opened');
+    });
+  });
+
+  describe('onCollapseAnimationDone', () => {
+    it('should scrollToCardElement on toState `opened`', () => {
+      spyOn(component as any, 'scrollToCardElement');
+      component.onCollapseAnimationDone({toState: 'opened'} as any);
+      expect((component as any).scrollToCardElement).toHaveBeenCalled();
+    });
+
+    it('should not scrollToCardElement on toState `closed`', () => {
+      spyOn(component as any, 'scrollToCardElement');
+      component.onCollapseAnimationDone({toState: 'closed'} as any);
+      expect((component as any).scrollToCardElement).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('scrollToCardElement', () => {
+    it('should scrollIntoView', () => {
+      spyOn((component as any).cardElement.nativeElement, 'scrollIntoView');
+      (component as any).scrollToCardElement();
+      expect((component as any).cardElement.nativeElement.scrollIntoView).toHaveBeenCalledWith(
+        {behavior: 'smooth', block: 'start'}
+      );
+    });
+  });
+
+  describe('chevronRotation', () => {
+    it('should return `180` if isCollapsed true', () => {
+      component.isCollapsed = true;
+      expect(component.chevronRotation).toBe(180);
+    });
+
+    it('should return `` if isCollapsed false', () => {
+      component.isCollapsed = false;
+      expect(component.chevronRotation).toBe('' as any);
+    });
+  });
 });
