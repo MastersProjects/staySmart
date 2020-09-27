9e1b86788f235b28967099b3a33e372076b41035
Chiramet Phong Penglerd <Phong@hotmail.ch>
2020-09-28 00:09:55 +0200

    setup firestore rules test suite: example [todo #59]

--- firebase-test/test.js
+++ firebase-test/test.js
@@ -1,7 +1,17 @@
 const assert = require('assert');
+const firebase = require('@firebase/testing');
 
 describe('Firebase Rules', () => {
+
+  const getFirestore = (auth) => firebase.initializeTestApp({projectId: 'staysmart-dev', auth}).firestore();
+
   it('should test example', () => {
     assert.strictEqual(1 + 1, 2);
+  });
+
+  it('should test firestore rules example', async () => {
+    const firestore = getFirestore(null);
+    const testDoc = firestore.collection('Configurations').doc('Configuration');
+    await firebase.assertSucceeds(testDoc.get());
   });
 });
